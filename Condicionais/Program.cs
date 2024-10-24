6663737c6c903b4e5b959fdca4725990c6baa7ba
BEATRIZ DORINE YUSMY DA SILVA <40511347847@info.local>
2024-10-24 14:38:56 -0300

    aula backend

---
+++ Condicionais/Program.cs
@@ -0,0 +1,13 @@
+﻿float precoProduto = 200;
+float minhaCarteira = 350;
+
+//criar um programa que responda se eu consigo comprar o produto? SIM OU NAO
+
+if (minhaCarteira >= precoProduto)
+{
+    console.WriteLine ($"Sim, voce pode comprar o produto");
+}
+else
+{
+    Console.WriteLine(@"Infelizmente voce NAO pode comprar");
+}

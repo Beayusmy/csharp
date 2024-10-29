ac3efdd7b6193f0fd056291322014c68d1a8f431
BEATRIZ DORINE YUSMY DA SILVA <40511347847@info.local>
2024-10-29 14:30:49 -0300

    aula 26

---
+++ Arrays/Program.cs
@@ -0,0 +1,10 @@
+﻿string[] carros = new string[3];
+
+for (int i = 0; i < 3; i++)
+
+{
+    Console.WriteLine($"Digite o nome do carro a ser cadastrado:");
+    carros [i] = Console.ReadLine()!;
+}
+
+Console.WriteLine ($"0 carro cadastrado e {carros [0]}, {carros [1]}, {carros [2]} ");           

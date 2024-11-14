62ceb6b4de85a5d369136d789557873de5bb02f0
BEATRIZ DORINE YUSMY DA SILVA <40511347847@info.local>
2024-11-14 15:03:15 -0300

    atualizacao dia 14 do 11

---
+++ Foreach01/Program.cs
@@ -0,0 +1,9 @@
+﻿string[] frutas = {"Melancia", "Carambola", "Maracuja", "Morango", "Limao", "Jabuticaba", "Abacate", "Kiwi", "Acerola", "Pinha", "Lichia", "Pitaya", "Pessego", "Banana" };
+
+Console.WriteLine($"Sua sacola contem {frutas.Length} fruta: ");
+
+foreach (string f in frutas)
+{
+    Console.WriteLine($"{f}, ");
+}
+ Console.WriteLine();
